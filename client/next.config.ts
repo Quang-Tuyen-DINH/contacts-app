e7d34e4f3afd1453a2539142af7774d004864756
Quang Tuyen Dinh <qtuyen43@gmail.com>
2025-11-02 15:28:37 +0100

    feat: add API route in client

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [{ source: "/api/:path*", destination: "http://localhost:5000/:path*" }];
+  },
 };
 
 export default nextConfig;
